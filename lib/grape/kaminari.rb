b07c70393a194d7bd0d5d22ac38dbbd884c913a4
Adrian Perez <adrianperez.deb@gmail.com>
2014-03-25 10:19:08 +0100

    Annotation fix

--- lib/grape/kaminari.rb
+++ lib/grape/kaminari.rb
@@ -15,15 +15,18 @@ module Grape
             end
           end
         end
+      end
+      base.extend(ClassMethods)
+    end
 
-        def self.paginate(options = {})
-          options.reverse_merge!(per_page: 10)
-          params do
-            optional :page,     type: Integer, default: 1,
-                                desc: 'Page offset to fetch.'
-            optional :per_page, type: Integer, default: options[:per_page],
-                                desc: 'Number of results to return per page.'
-          end
+    module ClassMethods
+      def paginate(options = {})
+        options.reverse_merge!(per_page: 10)
+        params do
+          optional :page,     type: Integer, default: 1,
+            desc: 'Page offset to fetch.'
+          optional :per_page, type: Integer, default: options[:per_page],
+            desc: 'Number of results to return per page.'
         end
       end
     end
